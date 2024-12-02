25a226978b73a6521ebd7eaa0d743062e803521c
cdelgado813 <cdelgado813@gmail.com>
2024-12-02 20:01:42 +0100

    Refactored imports
    Refactored imports

--- price-api/src/test/java/com/test/priceapi/PriceApiTest.java
+++ price-api/src/test/java/com/test/priceapi/PriceApiTest.java
@@ -6,9 +6,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.test.priceapi.application.port.PriceService;
 import com.test.priceapi.domain.model.Price;
-import com.test.priceapi.domain.port.out.PriceRepository;
 import com.test.priceapi.domain.service.PriceDomainService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
